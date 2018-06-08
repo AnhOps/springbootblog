5d578d398bf6a7fb177170d4748611a7c110b566
Quoc Anh <anhngoquoc@gmail.com>
2018-06-08 23:45:48 +0700

    added home

---
+++ src/main/java/com/opsbin/springbootblog/SpringbootblogApplication.java
@@ -0,0 +1,12 @@
+package com.opsbin.springbootblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootblogApplication.class, args);
+	}
+}
